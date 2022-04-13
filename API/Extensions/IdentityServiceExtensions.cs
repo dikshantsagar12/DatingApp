7199213e4bbf87414bd4c8d8416c4c2f16c23bc2
dikshantsagar12 <dikshantsagar12@gmail.com>
2022-04-13 15:41:48 +0530

    End of section 16

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
@@ -12,6 +15,15 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt =>{
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddRoleValidator<RoleValidator<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
@@ -22,6 +34,11 @@ namespace API.Extensions
                     ValidateIssuer = false,              //API server
                     ValidateAudience = false,            // angular app
                 };
+            });
+
+            services.AddAuthorization(opt=>{
+                opt.AddPolicy("RequireAdminRole", policy=> policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy=> policy.RequireRole("Admin","Moderator"));
             });
 
             
